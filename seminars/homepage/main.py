f4c967d1aaffb3dee2c7b3501f99ded253cf1aa5
Edgar Costa <edgarc@mit.edu>
2020-04-02 11:43:00 -0400

    reformating search

--- seminars/homepage/main.py
+++ seminars/homepage/main.py
@@ -1,7 +1,6 @@
-
 from seminars.app import app
 from seminars import db
-from seminars.talk  import WebTalk, talks_search, talks_lucky
+from seminars.talk import WebTalk, talks_search, talks_lucky
 from seminars.seminar import seminars_lucky
 from seminars.utils import basic_top_menu, categories
 from seminars.institution import institutions, WebInstitution
@@ -11,82 +10,98 @@ import datetime
 import pytz
 
 from lmfdb.utils import (
-    SearchArray, TextBox, SelectBox, YesNoBox,
-    to_dict, search_wrap,
+    SearchArray,
+    TextBox,
+    SelectBox,
+    YesNoBox,
+    to_dict,
+    search_wrap,
 )
+
 
 def get_now():
     # Returns now in UTC, comparable to time-zone aware datetimes from the database
     return datetime.datetime.now(pytz.UTC)
 
+
 class SemSearchArray(SearchArray):
     noun = "seminar"
     plural_noun = "seminars"
+
     def __init__(self):
         category = SelectBox(
-            name="category",
-            label="Category",
-            options=[("", "")] + categories())
-        keywords = TextBox(
-            name="keywords",
-            label="Keywords")
-        speaker = TextBox(
-            name="speaker",
-            label="Speaker")
-        affiliation = TextBox(
-            name="affiliation",
-            label="Affiliation")
-        institution = TextBox(
+            name="category", label="Category", options=[("", "")] + categories()
+        )
+        speaker = TextBox(name="speaker",
+                          label="Speaker",
+                          colspan=(1,2,1),
+                          width=160*2 - 1*20)
+        affiliation = TextBox(name="affiliation",
+                              label="Affiliation",
+                              colspan=(1,2,1),
+                              width=160*2 - 1*20)
+        institutions_shortnames = [(elt, elt) for elt in sorted(db.institutions.search({}, projection="shortname"))]
+        institutions_options = [("", ""), ("No institution", "None")] + institutions_shortnames
+        institution = SelectBox(
             name="institution",
-            label="Institution")
-        title = TextBox(
-            name="title",
-            label="Title")
+            label="Institution",
+            options=institutions_options
+        )
+        title = TextBox(name="title",
+                        label="Title",
+                        colspan=(1,2,1),
+                        width=160*2 - 1*20)
+        keywords = TextBox(name="keywords",
+                           label="Keywords",
+                           colspan=(1,2,1),
+                           width=160*2 - 1*20)
         online = SelectBox(
             name="online",
             label="Online",
-            options=[("", "only"),
-                     ("all", "and offline"),
-                     ("exclude", "exclude")])
-        when = SelectBox(
-            name="when",
-            label="Occurring in",
-            options=[("", ""),
-                     ("future", "the future"),
-                     ("past", "the past")])
-        date = TextBox( # should have date widget?
+            options=[("", "only"), ("all", "and offline"), ("exclude", "exclude")],
+        )
+        date = TextBox(  # should have date widget?
             name="daterange",
             id="daterange",
-            label="Date")
-        video = YesNoBox(
-            name="video",
-            label="Has video")
-        avail = SelectBox(
-            name="access",
-            label="Access",
-            options=[("", ""),
-                     ("open", "open only")])
-        count = TextBox(
-            name="count",
-            label="Results to display",
-            example=50)
-        self.browse_array = [[category, keywords], [speaker, affiliation], [title, institution], [when, date], [online], [video, avail], [count]]
+            label="Date",
+            colspan=(1, 2, 1),
+            width=160*2 - 1*20,
+        )
+        video = YesNoBox(name="video", label="Has video")
+        access = SelectBox(
+            name="access", label="Access", options=[("", ""), ("open", "open only")]
+        )
+        count = TextBox(name="count", label="Results to display", example=50)
+        self.browse_array = [
+            [category, keywords],
+            [institution, title],
+            [online, speaker],
+            [video, affiliation],
+            [access, date],
+            [count],
+        ]
+
 
 @app.route("/")
 def index():
     # Eventually want some kind of cutoff on which talks are included.
     # Deal with time zone right
-    talks = talks_search({'display':True, 'end_time':{'$gte':datetime.datetime.now()}}, sort=["start_time"])
+    talks = talks_search(
+        {"display": True, "end_time": {"$gte": datetime.datetime.now()}},
+        sort=["start_time"],
+    )
     menu = basic_top_menu()
     menu[0] = ("#", "$('#filter-menu').slideToggle(400); return false;", "Filter")
     return render_template(
-        'browse.html',
+        "browse.html",
         title="Math Seminars",
         info={},
         categories=categories(),
         talks=talks,
         top_menu=menu,
-        bread=None)
+        bread=None,
+    )
+
 
 @app.route("/search")
 def search():
@@ -105,7 +120,9 @@ def search():
         info=info,
         categories=categories(),
         top_menu=menu,
-        bread=None)
+        bread=None,
+    )
+
 
 @app.route("/institutions/")
 def list_institutions():
@@ -113,15 +130,17 @@ def list_institutions():
         "institutions.html",
         title="Institutions",
         institutions=institutions(),
-        top_menu=basic_top_menu())
+        top_menu=basic_top_menu(),
+    )
+
 
 @app.route("/seminar/<shortname>")
 def show_seminar(shortname):
-    seminar = seminars_lucky({'shortname': shortname})
+    seminar = seminars_lucky({"shortname": shortname})
     if seminar is None:
         return render_template("404.html", title="Seminar not found")
-    organizers = list(db.seminar_organizers.search({'seminar_id': shortname}))
-    talks = talks_search({'display':True, 'seminar_id': shortname}, projection=3)
+    organizers = list(db.seminar_organizers.search({"seminar_id": shortname}))
+    talks = talks_search({"display": True, "seminar_id": shortname}, projection=3)
     now = get_now()
     future = []
     past = []
@@ -139,11 +158,13 @@ def show_seminar(shortname):
         past=past,
         seminar=seminar,
         top_menu=basic_top_menu(),
-        bread=None)
+        bread=None,
+    )
+
 
 @app.route("/talk/<semid>/<int:talkid>/")
 def show_talk(semid, talkid):
-    talk = talks_lucky({'seminar_id': semid, 'seminar_ctr': talkid})
+    talk = talks_lucky({"seminar_id": semid, "seminar_ctr": talkid})
     if talk is None:
         return render_template("404.html", title="Talk not found")
     utcoffset = int(talk.start_time.utcoffset().total_seconds() / 60)
@@ -152,7 +173,9 @@ def show_talk(semid, talkid):
         title="View talk",
         talk=talk,
         utcoffset=utcoffset,
-        top_menu=basic_top_menu())
+        top_menu=basic_top_menu(),
+    )
+
 
 @app.route("/institution/<shortname>/")
 def show_institution(shortname):
@@ -164,7 +187,9 @@ def show_institution(shortname):
         "institution.html",
         title="View institution",
         institution=institution,
-        top_menu=basic_top_menu())
+        top_menu=basic_top_menu(),
+    )
+
 
 @app.route("/subscribe")
 def subscribe():
@@ -173,32 +198,31 @@ def subscribe():
     menu = basic_top_menu()
     menu.pop(2)
     return render_template(
-        "subscribe.html",
-        title="Subscribe",
-        top_menu=menu,
-        bread=None)
+        "subscribe.html", title="Subscribe", top_menu=menu, bread=None
+    )
     raise NotImplementedError
+
 
 @app.route("/about")
 def about():
     menu = basic_top_menu()
     menu.pop(4)
-    return render_template(
-        "about.html",
-        title="About",
-        top_menu=menu)
+    return render_template("about.html", title="About", top_menu=menu)
+
 
 @app.route("/<category>")
 def by_category(category):
     # raise error if not existing category?
-    return search({"category":category})
+    return search({"category": category})
 
-@search_wrap(template="seminar_search_results.html",
-             table=db.seminars,
-             title="Seminar Search Results",
-             err_title="Seminar Search Input Error",
-             bread=lambda:[("Search results", " ")])
+
+@search_wrap(
+    template="seminar_search_results.html",
+    table=db.seminars,
+    title="Seminar Search Results",
+    err_title="Seminar Search Input Error",
+    bread=lambda: [("Search results", " ")],
+)
 def search_seminars(info, query):
     # For now, just ignore the info and return all results
     pass
-
