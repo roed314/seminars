3ed970143f7031895acd6c65783fb393b3b5db58
Edgar Costa <edgarc@mit.edu>
2020-04-11 00:42:50 -0400

    username

--- configfiles/backup.sh
+++ configfiles/backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# have ~/.pgpass in your home directory
+# have ~/.pgpass in your home directory with permissions rw-------=600
 # with the format:
 # localhost:5432:beantheory:username:password
 
@@ -9,6 +9,6 @@ set -e
   echo Dumping beantheory
   echo timestamp = `date -u +%Y%m%d-%H%M`
   bdump=/scratch/beantheory/beans`date -u +%Y%m%d-%H%M`.tar
-  time pg_dump --host localhost --clean --if-exists --schema=public -v --file $bdump --format tar beantheory
+  time pg_dump -U lmfdb --host localhost --clean --if-exists --schema=public -v --file $bdump --format tar beantheory
   echo done
 set +e
