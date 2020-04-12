2b9f94fdf1c2e3a9defb27ccbdcf216d66c9f4e1
Edgar Costa <edgarc@mit.edu>
2020-04-12 12:25:07 -0400

    are you REALLY sure

--- seminars/__init__.py
+++ seminars/__init__.py
@@ -18,6 +18,16 @@ assert db
 def nothing(self, *args, **kwds):
     pass
 
+def are_you_REALLY_sure(func):
+    def call(*args, **kwargs):
+        ok = input("Are you REALLY sure you want to do call %s?\nYou will most likely break the website if you don't change the code first!! (yes/no)" % (func))
+        if not (ok and ok.lower() == "yes"):
+            return
+        else:
+            return func(*args, **kwargs)
+    return call
 
 for tname in db.tablenames:
     db[tname].log_db_change = nothing
+    db[tname].add_column = are_you_REALLY_sure(db[tname].add_column)
+    db[tname].drop_column = are_you_REALLY_sure(db[tname].drop_column)
