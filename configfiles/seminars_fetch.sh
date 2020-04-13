7b2fd65796c354e6299ad3155ee3071f427074e8
Edgar Costa <edgarc@mit.edu>
2020-04-13 17:40:26 -0400

    new files

---
+++ configfiles/seminars_fetch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+date
+
+for branch in stable master; do
+  if [ -d "/home/mathseminars/seminars-git-${branch}" ]; then
+    pushd /home/lmfdb/lmfdb-git-$branch
+    git fetch
+    git checkout origin/$branch -f
+    popd
+  fi
+done
+
+
+
+
+
