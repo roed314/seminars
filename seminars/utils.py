cacc20ff3b4b7b694badf03940972cc8228ed2eb
AndrewVSutherland <drew@math.mit.edu>
2020-04-16 18:58:03 -0400

    less red

--- seminars/utils.py
+++ seminars/utils.py
@@ -394,8 +394,10 @@ def process_user_input(inp, typ, tz):
 
 
 def format_errmsg (errmsg, *args):
-    """ Foramts an error message prefixed by "Error" (so don't start your errmsg with the word error) in red text with arguments in black """
     return Markup("Error: " + (errmsg % tuple("<span style='color:black'>%s</span>" % escape(x) for x in args)))
+
+def format_warning (errmsg, *args):
+    return Markup("Warning: " + (errmsg % tuple("<span style='color:black'>%s</span>" % escape(x) for x in args)))
 
 def show_input_errors(errmsgs):
     """ Flashes a list of specific user input error messages then displays a generic message telling the user to fix the problems and resubmit. """
