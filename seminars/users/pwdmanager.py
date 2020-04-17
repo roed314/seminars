048a3750d39e0faa295a6e1ed2e01cf31bd47a4b
David Roe <roed.math@gmail.com>
2020-04-17 17:07:50 -0400

    Fill in substitute emails when deleting user

--- seminars/users/pwdmanager.py
+++ seminars/users/pwdmanager.py
@@ -169,10 +169,10 @@ class PostgresUserTable(PostgresSearchTable):
         email = data["email"]
         with DelayCommit(db):
             # We probably have code that assumes that admin/owner isn't None....
-            db.institutions.update({"admin": ilike_query(email)}, {"admin": None})
-            db.seminars.update({"owner": ilike_query(email)}, {"owner": None})
+            db.institutions.update({"admin": ilike_query(email)}, {"admin": "mathseminars-dev@googlegroups.com"})
+            db.seminars.update({"owner": ilike_query(email)}, {"owner": "mathseminars-dev@googlegroups.com"})
             db.seminar_organizers.delete({"email": ilike_query(email)})
-            db.talks.update({"speaker_email": ilike_query(email)}, {"speaker_email": None})
+            db.talks.update({"speaker_email": ilike_query(email)}, {"speaker_email": ""})
             self.update({"id": uid}, {key: None for key in self.search_cols})
 
 userdb = PostgresUserTable()
