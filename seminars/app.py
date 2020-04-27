7e3b6ed76bc4070ce38e1dfe6bc457da97f18ea9
Edgar Costa <edgarc@mit.edu>
2020-04-27 17:05:31 -0400

    no talks

--- seminars/app.py
+++ seminars/app.py
@@ -114,7 +114,7 @@ def ctx_proc_userdata():
     # meta_description appears in the meta tag "description"
     data[
         "meta_description"
-    ] = r"Welcome to {topdomain}, a listing of research seminars, talks and conferences!".format(topdomain = topdomain())
+    ] = r"Welcome to {topdomain}, a listing of research seminars and conferences!".format(topdomain = topdomain())
     data[
         "feedbackpage"
     ] = r"https://docs.google.com/forms/d/e/1FAIpQLSdJNJ0MwBXzqZleN5ibAI9u1gPPu9Aokzsy08ot802UitiDRw/viewform"
