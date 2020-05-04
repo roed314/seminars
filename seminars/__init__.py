e21da95bdf1352dc5dc7e19ef3e01f2e9f380617
Edgar Costa <edgarc@mit.edu>
2020-05-03 22:18:22 -0400

    paching backend to stop using counts and stats

--- seminars/__init__.py
+++ seminars/__init__.py
@@ -10,6 +10,7 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../lmf
 # from .website import main
 # assert main
 from lmfdb.backend import db
+
 assert db
 
 # Have to make sure that changes aren't logged using the LMFDB's logging mechanism.
@@ -31,7 +32,26 @@ def are_you_REALLY_sure(func):
     return call
 
 
+def update(self, query, changes, resort=False, restat=False, commit=True):
+    return PostgresSearchTable.update(
+        self, query=query, changes=changes, resort=resort, restat=restat, commit=commit
+    )
+
+
+def count(self, query, record=False):
+    return PostgresSearchTable.count(self, query=query, record=record)
+
+
+def insert_many(self, data, resort=False, reindex=False, restat=False, commit=True):
+    return PostgresSearchTable.insert_many(
+        self, data=data, resort=resort, reindex=reindex, restat=restat, commit=commit
+    )
+
+
 for tname in db.tablenames:
     db[tname].log_db_change = nothing
     db[tname].add_column = are_you_REALLY_sure(db[tname].add_column)
     db[tname].drop_column = are_you_REALLY_sure(db[tname].drop_column)
+    db[tname].update = update
+    db[tname].count = count
+    db[tname].insert_many = insert_many
