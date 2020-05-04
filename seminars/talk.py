3af83a2e307c6be216bf18931c899a9732497060
AndrewVSutherland <drew@math.mit.edu>
2020-05-04 13:44:15 -0400

    fix constructor in talks

--- seminars/talk.py
+++ seminars/talk.py
@@ -622,7 +622,7 @@ def _construct(seminar_dict, projecting=False):
             seminar=seminar_dict.get(rec["seminar_id"]),
             data=rec,
         )
-    def projection_contruct(rec):
+    def projection_construct(rec):
         return rec
 
     return inner_construct if not projecting else projection_construct
