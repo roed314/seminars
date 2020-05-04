6d5458e06affca29f12d5804895ab1c5864340b7
AndrewVSutherland <drew@math.mit.edu>
2020-05-04 18:13:00 -0400

    debug

--- seminars/create/main.py
+++ seminars/create/main.py
@@ -403,6 +403,11 @@ def save_seminar():
         errmsgs.append(format_input_errmsg("integer must be positive", data["per_day"], "per_day"))
     if seminar.is_conference and (not data["start_date"] or not data["end_date"]):
         errmsgs.append("Please specify the start and end dates of your conference (you can change these later if needed).")
+    else:
+        if seminar.is_conference:
+            print(data["start_date"])
+            print(not data["start_date"])
+
     if seminar.is_conference and not data["per_day"]:
         flash_warning ("It will be easier to edit the conference schedule if you specify talks per day (an upper bound is fine).")
 
