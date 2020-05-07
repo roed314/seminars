6fe9a3b3c8fff5b1756b7da7429c26e874e0faf5
Edgar Costa <edgarc@mit.edu>
2020-05-07 12:01:18 -0400

    toggle3way

--- seminars/utils.py
+++ seminars/utils.py
@@ -556,8 +556,34 @@ def toggle(tglid, value, checked=False, classes="", onchange="", name=""):
     if classes:
         classes += " "
     return """
-<input type="checkbox" class="{classes}tgl tgl-light" value="{value}" id="{tglid}" onchange="{onchange}" name="{name}" {checked}>
+<input
+    type="checkbox"
+    class="{classes} tgl tgl-light"
+    value="{value}"
+    id="{tglid}"
+    onchange="{onchange}"
+    name="{name}"
+    {checked}>
 <label class="tgl-btn" for="{tglid}"></label>
+""".format(
+        tglid=tglid, value=value, checked="checked" if checked else "", classes=classes, onchange=onchange, name=name,
+    )
+
+
+def toggle3way(tglid, value, classes="", onchange="", name=""):
+    if classes:
+        classes += " "
+    return """
+<input
+    class="{classes} tgl tgl-light"
+    value="{value}" id="{tglid}"
+    onchange="{onchange}"
+    name="{name}">
+<label
+    class="tgl-btn"
+    for="{tglid}"
+    onclick="this.control.value = ((parseInt(this.control.value) + 2)%3) - 1;this.control.dataset.chosen=this.control.value;">
+></label>
 """.format(
         tglid=tglid, value=value, checked="checked" if checked else "", classes=classes, onchange=onchange, name=name,
     )
