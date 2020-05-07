d7875061c03ee22a67aaa536280961d106db67d0
Edgar Costa <edgarc@mit.edu>
2020-05-07 16:04:31 -0400

    moving toggles

--- seminars/toggle.py
+++ seminars/toggle.py
@@ -1,2 +1,47 @@
-# We want to move these here eventually but for now Edgar is editing them in utils
-from seminars.utils import toggle, toggle3way, Toggle
+def toggle(tglid, value, checked=False, classes="", onchange="", name=""):
+    if classes:
+        classes += " "
+    return """
+<input
+    type="checkbox"
+    class="{classes}tgl tgl-light"
+    value="{value}"
+    id="{tglid}"
+    onchange="{onchange}"
+    name="{name}"
+    {checked}
+    ></input>
+<label class="tgl-btn" for="{tglid}"></label>
+""".format(
+        tglid=tglid,
+        value=value,
+        checked="checked" if checked else "",
+        classes=" " + classes if classes else "",
+        onchange=onchange,
+        name=name,
+    )
+
+
+def toggle3way(tglid, value, classes="", onchange="", name=""):
+    if classes:
+        classes += " "
+    return """
+<input
+    class="{classes}tgl tgl-light"
+    value="{value}" id="{tglid}"
+    onchange="{onchange}"
+    name="{name}"
+    ></input>
+<label
+    class="tgl-btn"
+    for="{tglid}"
+    onclick="this.control.value = ((parseInt(this.control.value) + 2)%3) - 1;this.control.dataset.chosen=this.control.value;this.control.onchange();"
+    >
+</label>
+""".format(
+        tglid=tglid,
+        value=value,
+        classes=" " + classes if classes else "",
+        onchange=onchange,
+        name=name,
+    )
