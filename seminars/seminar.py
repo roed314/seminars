b6921c8aa3f5df12d3d492856f9f1f6777d50071
AndrewVSutherland <drew@math.mit.edu>
2020-05-13 10:25:31 -0400

    schema changes, name --> full_name

--- seminars/seminar.py
+++ seminars/seminar.py
@@ -112,7 +112,6 @@ class WebSeminar(object):
                 db.seminar_organizers.search({"seminar_id": self.shortname}, sort=["order"])
             )
         self.organizers = organizers
-        print(self.organizers)
         self.cleanse()
 
     def __repr__(self):
