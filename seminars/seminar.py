c5a97bda68db3aca7dfec7a3e8269d97b405dcdd
AndrewVSutherland <drew@math.mit.edu>
2020-05-16 14:56:16 -0400

    new talks and save talk validation

--- seminars/seminar.py
+++ seminars/seminar.py
@@ -221,7 +221,7 @@ class WebSeminar(object):
         if self.online and self.access_control is None:
             self.access_control = 0 if self.access == 'open' else self.access_control
             self.access_control = 3 if self.access in ['users', 'endorsed'] else self.access_control
-        if "comments" in self.live_link"
+        if "comments" in self.live_link:
             self.live_link = ""
         # remove columns we plan to drop
         for attr in ["start_time","end_time","start_times","end_times","weekday","archived"]:
