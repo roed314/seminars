fff40e5e2c6a5952a61f4e3b86c6dc60cdf1eecd
AndrewVSutherland <drew@math.mit.edu>
2020-05-17 20:08:10 -0400

    cleanse

--- seminars/talk.py
+++ seminars/talk.py
@@ -113,6 +113,11 @@ class WebTalk(object):
         if self.online and self.access_control is None:
             self.access_control = 0 if self.access == 'open' else self.access_control
             self.access_control = 3 if self.access in ['users', 'endorsed'] else self.access_control
+            if "comments" in self.live_link:
+                self.live_link = ""
+                if self.homepage:
+                    self.access_control = 5
+                    self.access_registration = self.homepage
         if self.online and self.live_link and "comments" in self.live_link:
             self.live_link = ""
         # remove columns we plan to drop
