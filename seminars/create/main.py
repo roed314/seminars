75b1a6348b30aa6aaf6efa111a7898f4f3fbf54f
AndrewVSutherland <drew@math.mit.edu>
2020-05-24 06:15:08 -0400

    fix bug in process_save_talk

--- seminars/create/main.py
+++ seminars/create/main.py
@@ -916,7 +916,7 @@ def process_save_talk(talk, raw_data, warn=flash_warnmsg, format_error=format_er
 
     # Don't try to create new_version using invalid input
     if errmsgs:
-        return show_input_errors(errmsgs)
+        return data. errmsgs
     else:  # to make it obvious that these two statements should be together
         new_version = WebTalk(talk.seminar_id, data=data)
 
