0999077da9615ca9a4e2d1e2fc16407483377c32
David Roe <roed.math@gmail.com>
2020-06-07 04:08:42 -0400

    Update style

--- seminars/homepage/main.py
+++ seminars/homepage/main.py
@@ -470,7 +470,7 @@ def _get_row_attributes(objects):
     visible_counter = 0
     for obj in objects:
         classes, filtered = filter_classes(obj)
-        if isinstance(obj, WebTalk) and adapt_datetime(obj.start_time, current_user.tz) == PROTEST_DATE and obj.rescheduled():
+        if isinstance(obj, WebTalk) and adapt_datetime(obj.start_time, current_user.tz).date() == PROTEST_DATE and obj.rescheduled():
             classes.append("blm")
         style = ""
         if filtered:
