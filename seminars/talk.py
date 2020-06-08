85757d62d2ac653332ceb711b65d17fc0f7594f7
Edgar Costa <edgarc@mit.edu>
2020-06-08 18:02:30 -0400

    undo

--- seminars/talk.py
+++ seminars/talk.py
@@ -362,7 +362,7 @@ class WebTalk(object):
                 content=Markup.escape(render_template("talk-knowl.html", talk=self, _external=_external, tz=tz)),
             )
         else:
-            return r'<a title="{title}" knowl="talk/{seminar_id}/{talkid}"><span {style}>{title}</span>{rescheduled}</a>'.format(
+            return r'<a title="{title}" knowl="talk/{seminar_id}/{talkid}" {style}>{title}</a>{rescheduled}'.format(
                 title=self.show_title(),
                 style='style="text-decoration: line-through;font-style: italic;"' if rescheduled else '',
                 seminar_id=self.seminar_id,
