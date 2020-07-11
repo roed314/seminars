0f24cfc28860ff71a426b687964845cac76cf61b
AndrewVSutherland <drew@math.mit.edu>
2020-07-11 14:29:34 -0400

    tweak password checking

--- seminars/static/seminars.js
+++ seminars/static/seminars.js
@@ -914,7 +914,7 @@ function checkpw() {
   var len = $("#pw1").val().length;
   $("#pw2status").html("");
   if (len==0) $("#pwstatus").html("Don't use a password that you use elsewhere!");
-  if (len > 0 && < 8){
+  if (len > 0 && len < 8){
     $("#pw1status").html("Too short (less than 8 characters)");
   } else {
     $("#pw1status").html("");
