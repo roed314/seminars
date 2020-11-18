25694cc5071eef291386626217bde0e964af3da8
Edgar Costa <edgarc@mit.edu>
2020-11-17 21:52:07 -0500

    new default limit

--- seminars/homepage/main.py
+++ seminars/homepage/main.py
@@ -389,7 +389,7 @@ def index():
 
 
 # we need two functions because of url_for calls
-default_limit=1000
+default_limit=500
 @app.route("/talks", defaults={'limit': default_limit, 'timestamp': None})
 @app.route("/talks/<int:timestamp>", defaults={'limit': default_limit})
 @app.route("/talks/<int:timestamp>/<int:limit>")
