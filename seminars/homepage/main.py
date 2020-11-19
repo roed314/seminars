de9b19ab11e8dbcd1b87b09633263b106740941c
Edgar Costa <edgarc@mit.edu>
2020-11-19 09:29:54 -0500

    typo

--- seminars/homepage/main.py
+++ seminars/homepage/main.py
@@ -651,7 +651,7 @@ def _talks_index(query={},
                     t0, t1 = date_and_daytimes_to_times(talkstart.date(), timerange, tz)
                     talk.more = (t0 <= talkstart) and (talkend <= t1)
     # get last_time before throwing away talks
-    last_time = int(talks[-1].start_time.timestamp()) if talks else None,
+    last_time = int(talks[-1].start_time.timestamp()) if talks else None
     if fully_filtered:
         row_attributes, talks = _get_row_attributes(talks, visible_counter, fully_filtered)
     else:
