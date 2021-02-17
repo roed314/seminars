9e3cf3f39af9f43864fe2625bf0ebb7886ec231d
Edgar Costa <edgarc@mit.edu>
2021-02-17 16:13:48 +0000

    remove banner

--- seminars/config.py
+++ seminars/config.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
-# LMFDB - L-function and Modular Forms Database web-site - www.lmfdb.org
-# Copyright (C) 2010-2012 by the LMFDB authors
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-
+# forked from https://github.com/LMFDB/lmfdb/blob/master/lmfdb/utils/config.py
 """
 This file must not depend on other files from this project.
 It's purpose is to parse a config file (create a default one if none
